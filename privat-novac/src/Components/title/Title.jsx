c81e25fae20473306ae1fbae6cf77a612afbf6f3
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-09-22 12:26:18 +0200

    Create Part of maps and contact

--- privat-novac/src/Components/title/Title.jsx
+++ privat-novac/src/Components/title/Title.jsx
@@ -1,9 +1,16 @@
 import './Title.css';
 
-export const Title = ({children}) => {
+export const Title = ({ children, variant }) => {
+    const frameClass =
+        variant === 'primary'
+            ? 'frame-white'
+            : variant === 'secondary'
+            ? 'frame-blue'
+            : '';
+
     return (
-        <div className="title-container ">
+        <div className={`title-container ${frameClass}`}>
             <div className="title">{children}</div>
         </div>
     );
-}   
+}
