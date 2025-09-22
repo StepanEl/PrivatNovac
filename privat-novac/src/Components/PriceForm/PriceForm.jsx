f9c65407babe6c50316b93a2a7197a048ec384fa
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-09-22 09:20:55 +0200

    Create priceForm

--- privat-novac/src/Components/PriceForm/PriceForm.jsx
+++ privat-novac/src/Components/PriceForm/PriceForm.jsx
@@ -1,12 +1,47 @@
 import './PriceForm.css';
 import { useState } from 'react';
 
+const priceTable = {
+  1: { 
+    1: 800,
+    2: 1200,
+    3: 1650,
+    4: 2120
+  },
+  2: { 
+    1: 800,
+    2: 1100,
+    3: 1410,
+    4: 1880
+  },
+  3: { 
+    1: 700,
+    2: 1000,
+    3: 1350,
+    4: 1800
+  }
+};
+
+  
+const getPrice = (persons, nights) => {
+  const key = nights >= 3 ? 3 : nights;  
+
+  if (persons <= 4) {
+    return priceTable[key][persons] * nights;
+  } else {
+    const basePricePerNight = priceTable[key][4];
+    const onePersonPricePerNight = basePricePerNight / 4;
+    const extraPersons = persons - 4;
+    return (basePricePerNight + extraPersons * onePersonPricePerNight) * nights;
+  }
+};
 
 export const PriceForm = () => {
-    const [nights, setNights] = useState(1);
-    const [persons, setPersons] = useState(1);
+  const [nights, setNights] = useState(1);
+  const [persons, setPersons] = useState(1);
 
-    const price = nights * persons * 500;
+  const price = getPrice(persons, nights);
+
 
     return (
         <>
--- privat-novac/src/Components/title/Title.jsx
+++ privat-novac/src/Components/title/Title.jsx
@@ -3,7 +3,7 @@ import './Title.css';
 export const Title = ({children}) => {
     return (
         <div className="title-container ">
-            <p className="title">{children}</p>
+            <div className="title">{children}</div>
         </div>
     );
 }   
